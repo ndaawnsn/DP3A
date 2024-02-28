c099c2391539de5475d16620bdb330b2a3bdd582
Tampzzz <jeremiatampi24@gmail.com>
2024-02-28 10:48:39 +0800

    pelaporan

--- inc/carousel.php
+++ inc/carousel.php
@@ -7,7 +7,7 @@
        <span class="hero-maintittle">Dinas Pemberdayaan Perempuan <br> Dan Anak Kota Manado<br /></span><br />
        <span class="hero-subtittle">Stop Kekerasan Pada Perempuan Dan Anak</span>
      </div>
-     <a href="#product" class="btn btn-hero" style="margin-right:50px;">SHOP NOW</a>
+     <a href="#product" class="btn btn-hero" style="margin-right:50px;">PELAPORAN</a>
    </div>
    <div class="cursor"></div>
    <div class="cursor2"></div>
