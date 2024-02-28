4f3e5e6141dbe3d0aff522798d90b12053749e68
Tampzzz <jeremiatampi24@gmail.com>
2024-02-28 10:50:47 +0800

    GAMING,SLMT DTG

--- inc/carouselGaming.php
+++ inc/carouselGaming.php
@@ -1,16 +1,14 @@
  <!-- HERO START -->
  <div class="hero">
-        <div class="hero-wrap">
-          <div class="hero-tittle">
-            Laptop For Gaming<br />
-            <hr
-              style="width: 38%; margin-top: 5px; border: 1px solid #00ffc2"
-            />
-            <span class="hero-maintittle">5TECH - GAMING</span><br />
-            <span class="hero-subtittle">Up To 40% Off Next Season</span>
-          </div>
-          <!-- <a href="" class="btn btn-hero">SHOP NOW</a> -->
-        </div>
-        <div class="cursor"></div>
-        <div class="cursor2"></div>
-      </div>
+   <div class="hero-wrap">
+     <div class="hero-tittle">
+       Laptop For Gaming<br />
+       <hr style="width: 38%; margin-top: 5px; border: 1px solid #00ffc2" />
+       <span class="hero-maintittle">SEALMAT DATANG</span><br />
+       <span class="hero-subtittle">Up To 40% Off Next Season</span>
+     </div>
+     <!-- <a href="" class="btn btn-hero">SHOP NOW</a> -->
+   </div>
+   <div class="cursor"></div>
+   <div class="cursor2"></div>
+ </div>
